8d76e2fa06283ae4f38be255a9228bda352b3248
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-08 14:18:32 -0300

    chore: change DataSource config

--- server/src/database/DataSource.ts
+++ server/src/database/DataSource.ts
@@ -2,24 +2,31 @@ import path from "path";
 import { DataSource } from "typeorm";
 import { __TEST__ } from "../constants";
 
+const selectedDatabasePort: number = process.env.DB_PORT
+  ? parseInt(process.env.DB_PORT, 10)
+  : 5432;
+
 const dataSource = new DataSource({
   name: "default",
-  type: "postgres" as const,
-  host: process.env.DB_HOST,
-  port: +process.env.DB_PORT!,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE || ":memory:",
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: selectedDatabasePort,
+  username: process.env.DB_USERNAME || "default",
+  password: process.env.DB_PASSWORD || "default",
+  database: process.env.DB_DATABASE || "default",
   logging: true,
   synchronize: true,
   migrationsRun: true,
   dropSchema: __TEST__,
   entities: [
-    path.join(__dirname, "..", "entity", "**", "*.{ts,js}"),
-    path.join(__dirname, "..", "entity", "*.{ts,js}"),
+    path.join(__dirname, "..", "entities", "**", "*.{ts,js}"),
+    path.join(__dirname, "..", "entities", "*.{ts,js}"),
   ],
   migrations: [
     path.join(__dirname, "..", "database", "migrations", "*.{ts,js}"),
+  ],
+  subscribers: [
+    path.join(__dirname, "..", "database", "subscribers", "*.{ts,js}"),
   ],
 });
 
