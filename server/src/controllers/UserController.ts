23f82d7cefa43a0d18d4653e87463a48191e78aa
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-09 09:01:08 -0300

    build: install uui types for validate, adjusted UserController

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import * as bcrypt from "bcrypt";
 import { QueryFailedError } from "typeorm";
+import { validate } from "uuid";
 import { userRepository } from "../repositories/userRepository";
 
 interface CreateUserRequestBody {
@@ -14,14 +15,16 @@ export class UserController {
     const { username, password, email } = req.body as CreateUserRequestBody;
 
     if (!username || !password || !email) {
-      return res.status(400).send("All fields are required.");
+      return res.status(400).json({ message: "All fields are required." });
     }
 
     const existingUserByEmail = await userRepository.findOneBy({ email });
     const existingUserByUsername = await userRepository.findOneBy({ username });
 
     if (existingUserByEmail || existingUserByUsername) {
-      return res.status(409).send("Username or Email already exists.");
+      return res
+        .status(409)
+        .json({ message: "Username or Email already exists." });
     }
 
     const isPasswordStrong = (password: string): boolean => {
@@ -31,7 +34,9 @@ export class UserController {
     };
 
     if (!isPasswordStrong(password)) {
-      return res.status(400).send("Password does not meet the criteria.");
+      return res
+        .status(400)
+        .json({ message: "Password does not meet the criteria." });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -46,11 +51,13 @@ export class UserController {
       await userRepository.save(newUser);
 
       const repsonseUser = { ...newUser, password: undefined };
-      return res.status(201).send(repsonseUser);
+      return res.status(201).json(repsonseUser);
     } catch (error) {
       console.error(error);
       if (error instanceof QueryFailedError) {
-        return res.status(409).send("Duplicate field value entered");
+        return res
+          .status(409)
+          .json({ message: "Duplicate field value entered" });
       }
       return res.status(500).json({ message: "Internal server error" });
     }
@@ -58,6 +65,10 @@ export class UserController {
 
   async getUser(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
+
+    if (!validate(id)) {
+      return res.status(400).json({ message: "Invalid user ID format." });
+    }
 
     try {
       const user = await userRepository.findOneBy({ id });
