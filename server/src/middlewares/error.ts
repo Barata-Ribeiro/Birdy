3119a214ce89af87808c862a59aeb62fe68bdbef
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-10 09:44:25 -0300

    refactor: finish middleware creation

--- server/src/middlewares/error.ts
+++ server/src/middlewares/error.ts
@@ -1,12 +1,13 @@
 import { Request, Response, NextFunction } from "express";
-import ApiError from "src/helpers/api-errors";
+import { ApiError } from "../helpers/api-errors";
 
 const errorMiddleware = (
   error: Error & Partial<ApiError>,
   _req: Request,
   res: Response,
-  next: NextFunction
-) => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _next: NextFunction
+): Response => {
   const statusCode = error.status ?? 500;
   const message = error.status ? error.message : "Internal Server Error";
 
