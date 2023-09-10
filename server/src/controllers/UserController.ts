01f8eeff1f0cf50fe616529f822213c85ebc33f0
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-10 09:57:04 -0300

    refactor: add error middleware to controller

--- server/src/controllers/UserController.ts
+++ server/src/controllers/UserController.ts
@@ -2,7 +2,14 @@ import { Request, Response } from "express";
 import * as bcrypt from "bcrypt";
 import { QueryFailedError } from "typeorm";
 import { validate } from "uuid";
+
 import { userRepository } from "../repositories/userRepository";
+import {
+  BadRequestError,
+  ConflictError,
+  InternalServerError,
+  NotFoundError,
+} from "src/helpers/api-errors";
 
 interface CreateUserRequestBody {
   username: string;
@@ -14,18 +21,14 @@ export class UserController {
   async createUser(req: Request, res: Response): Promise<Response> {
     const { username, password, email } = req.body as CreateUserRequestBody;
 
-    if (!username || !password || !email) {
-      return res.status(400).json({ message: "All fields are required." });
-    }
+    if (!username || !password || !email)
+      throw new BadRequestError("All fields are required.");
 
     const existingUserByEmail = await userRepository.findOneBy({ email });
     const existingUserByUsername = await userRepository.findOneBy({ username });
 
-    if (existingUserByEmail || existingUserByUsername) {
-      return res
-        .status(409)
-        .json({ message: "Username or Email already exists." });
-    }
+    if (existingUserByEmail || existingUserByUsername)
+      throw new ConflictError("User already exists.");
 
     const isPasswordStrong = (password: string): boolean => {
       const regex =
@@ -33,77 +36,60 @@ export class UserController {
       return regex.test(password);
     };
 
-    if (!isPasswordStrong(password)) {
-      return res
-        .status(400)
-        .json({ message: "Password does not meet the criteria." });
-    }
+    if (!isPasswordStrong(password))
+      throw new BadRequestError("Password is too weak.");
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
+    const newUser = userRepository.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
     try {
-      const newUser = userRepository.create({
-        username,
-        email,
-        password: hashedPassword,
-      });
-
       await userRepository.save(newUser);
-
-      const repsonseUser = { ...newUser, password: undefined };
-      return res.status(201).json(repsonseUser);
     } catch (error) {
       console.error(error);
-      if (error instanceof QueryFailedError) {
-        return res
-          .status(409)
-          .json({ message: "Duplicate field value entered" });
-      }
-      return res.status(500).json({ message: "Internal server error" });
+
+      if (error instanceof QueryFailedError)
+        throw new ConflictError("Duplicate field value entered.");
+
+      throw new InternalServerError("Internal server error");
     }
+
+    const responseUser = { ...newUser, password: undefined };
+    return res.status(201).json(responseUser);
   }
 
   async getUser(req: Request, res: Response): Promise<Response> {
     const { id } = req.params;
 
-    if (!validate(id)) {
-      return res.status(400).json({ message: "Invalid user ID format." });
-    }
+    if (!validate(id)) throw new BadRequestError("Invalid user ID format.");
 
-    try {
-      const user = await userRepository.findOneBy({ id });
+    const user = await userRepository.findOneBy({ id });
 
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
+    if (!user) throw new NotFoundError("User not found");
 
-      const responseUser = { ...user, password: undefined };
-      return res.status(200).json(responseUser);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: "Internal server error" });
-    }
+    const responseUser = { ...user, password: undefined };
+    return res.status(200).json(responseUser);
   }
 
   async getAllUsers(_req: Request, res: Response): Promise<Response> {
-    try {
-      const users = await userRepository.find();
-      const responseUsers = users.map((user) => {
-        return {
-          id: user.id,
-          username: user.username,
-          email: user.email,
-          createdAt: user.createdAt,
-          updatedAt: user.updatedAt,
-          comments: user.comments,
-          photos: user.photos,
-        };
-      });
+    const users = await userRepository.find();
 
-      return res.status(200).json(responseUsers);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: "Internal server error" });
-    }
+    const responseUsers = users.map((user) => {
+      return {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+        createdAt: user.createdAt,
+        updatedAt: user.updatedAt,
+        comments: user.comments,
+        photos: user.photos,
+      };
+    });
+
+    return res.status(200).json(responseUsers);
   }
 }
