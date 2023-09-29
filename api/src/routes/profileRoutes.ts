eda08b1c77147eae59cf4e2d4bbf946e982ac18a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-29 08:02:12 -0300

    refactor: change statsRoutes to profileRoutes

--- api/src/routes/profileRoutes.ts
+++ api/src/routes/profileRoutes.ts
@@ -1,15 +1,15 @@
 import express from "express";
 
-import { StatsController } from "../controllers/StatsController";
+import { ProfileController } from "../controllers/ProfileController";
 import { authMiddleware } from "../middlewares/auth";
 
 const router = express.Router();
 
-const statsController = new StatsController();
+const profileController = new ProfileController();
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 router.get("/", authMiddleware, (req, res, next) => {
-  statsController.getStats(req, res).catch(next);
+  profileController.getProfile(req, res).catch(next);
 });
 
 export default router;
