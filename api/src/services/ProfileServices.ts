4591f0843d1b8bcdc443886def4277544c602879
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-09-29 08:16:12 -0300

    fix: adjust query search in ProfileService

--- api/src/services/ProfileServices.ts
+++ api/src/services/ProfileServices.ts
@@ -6,7 +6,10 @@ import { NotFoundError } from "../helpers/api-errors";
 
 export class ProfileServices {
   static async getProfile(userId: string): Promise<ProfileResponseDTO> {
-    const actualUser = await userRepository.findOneBy({ id: userId });
+    const actualUser = await userRepository.findOne({
+      where: { id: userId },
+      relations: ["photos", "photos.meta"],
+    });
     if (!actualUser) throw new NotFoundError("User not found.");
 
     return ProfileResponseDTO.fromEntity(actualUser);
