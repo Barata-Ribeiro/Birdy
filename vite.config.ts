7b5ba3722929d136b22bab6dea79dadfbe874500
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-04 06:45:59 -0300

    chore: adjust svgr plugin config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import svgrPlugin from "vite-plugin-svgr";
+import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	publicDir: "public",
 	build: {
 		sourcemap: true,
 	},
-	plugins: [
-		react(),
-		svgrPlugin({
-			svgrOptions: {
-				icon: true,
-			},
-		}),
-	],
-	publicDir: "public",
+	plugins: [react(), svgr()],
 });
