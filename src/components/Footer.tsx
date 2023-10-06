010e6af188ef0c30becc8caa7bc2ea05760a6bbb
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-06 07:19:49 -0300

    feat: add Footer links and text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
+
 import MainButton from "./shared/MainButton";
 
 const footerLinkes = {
 	privacy: "/privacy-policy",
 	terms: "/terms-of-use",
-	github: "https://github.com/Barata-Ribeiro/Birdy",
+	portfolio: "https://barataribeiro.com/",
 };
 
 const Footer = () => {
@@ -21,27 +23,26 @@ const Footer = () => {
 					</Link>
 
 					<p className="mx-auto mt-4 max-w-md text-green-spring-500 dark:text-green-spring-400">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Birdy is an open-source project made by{" "}
+						<Link
+							className="text-green-spring-600 transition-colors duration-300 hover:text-bright-turquoise-500 dark:text-green-spring-400 dark:hover:text-bright-turquoise-200"
+							to={"https://barataribeiro.com/"}
+							rel="noopener noreferrer"
+						>
+							Barata Ribeiro
+						</Link>
+						. Feel free to use, study, or contribute as you wish.
 					</p>
 
 					<div className="mt-4 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-center">
 						<Link
 							to="https://github.com/Barata-Ribeiro/Birdy"
+							rel="noopener noreferrer"
 							className="order-1 mt-3 flex w-full items-center justify-center rounded-md border p-2 text-sm capitalize tracking-wide text-green-spring-600 transition-colors duration-300 hover:bg-green-spring-100 focus:outline-none focus:ring focus:ring-green-spring-300/40 dark:border-green-spring-400 dark:text-green-spring-300 dark:hover:bg-green-spring-800 sm:mt-0 sm:w-auto"
 						>
-							<svg
-								className="mx-1 h-5 w-5"
-								viewBox="0 0 24 24"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C21.9939 17.5203 17.5203 21.9939 12 22ZM4 12.172C4.04732 16.5732 7.64111 20.1095 12.0425 20.086C16.444 20.0622 19.9995 16.4875 19.9995 12.086C19.9995 7.68451 16.444 4.10977 12.0425 4.086C7.64111 4.06246 4.04732 7.59876 4 12V12.172ZM10 16.5V7.5L16 12L10 16.5Z"
-									fill="currentColor"
-								></path>
-							</svg>
+							<FaGithub size="18" />
 
-							<span className="mx-1">View Demo</span>
+							<span className="mx-1">Repository</span>
 						</Link>
 						<MainButton NavToLink={"/signup"} padding={"px-5 py-2"}>
 							Sign Up
