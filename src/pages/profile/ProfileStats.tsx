51552f90d9bd4332b4956294fb06d09abba5b8e5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-10-20 08:36:36 -0300

    feat: implement basic 'ProfileStats' route

--- src/pages/profile/ProfileStats.tsx
+++ src/pages/profile/ProfileStats.tsx
@@ -1,5 +1,24 @@
 const ProfileStats = () => {
-	return <div>ProfileStats</div>;
+	return (
+		<section className="p-4 sm:px-0">
+			<h1 className="text-center text-2xl">Your Stats</h1>
+			<article className="my-4 grid grid-cols-2 gap-6">
+				<div className="col-span-full flex justify-between rounded-lg p-4 font-heading font-medium shadow-lg">
+					<p>
+						Total Hits: <span className="font-body font-normal">2394879</span>
+					</p>
+					<p>
+						Total Comments: <span className="font-body font-normal">3456</span>
+					</p>
+					<p>
+						Total Likes: <span className="font-body font-normal">672</span>
+					</p>
+				</div>
+				<div className="col-auto rounded-lg p-4 font-heading font-medium shadow-lg"></div>
+				<div className="col-auto rounded-lg p-4 font-heading font-medium shadow-lg"></div>
+			</article>
+		</section>
+	);
 };
 
 export default ProfileStats;
