46873d1720c1b0b6ed60de0aba09746e5bbc1ba6
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-03 08:22:28 -0300

    fix: adjust props typing, fix error handling not showing css properly

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,53 +1,39 @@
-import { FC, InputHTMLAttributes } from "react";
+import { ComponentProps } from "react";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+type InputProps = ComponentProps<"input"> & {
 	label: string;
 	type: string;
 	name: string;
 	inputClasses: string;
-	placeholder?: string;
-	value?: string;
-	onChange?: React.ChangeEventHandler<HTMLInputElement>;
-	onBlur?: React.FocusEventHandler<HTMLInputElement>;
-	ariaInvalid?: boolean;
-	ariaDescribedby?: string;
-	error?: string;
-}
+	error?: string | null;
+};
 
-const Input: FC<InputProps> = ({
+const Input = ({
 	label,
 	type,
 	name,
 	inputClasses,
-	value,
-	onChange,
-	onBlur,
 	error,
-	ariaInvalid,
-	ariaDescribedby,
 	...props
-}) => {
+}: InputProps) => {
 	return (
 		<>
 			<label className="sr-only capitalize" htmlFor={name}>
 				{label}
 			</label>
 			<input
-				className={`${inputClasses} block w-full rounded-lg border-green-spring-50 text-mantis-950 focus:border-bright-turquoise-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-2 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500`}
+				className={`${inputClasses} peer block w-full rounded-lg border-green-spring-50 text-mantis-950 focus:border-bright-turquoise-500 ${
+					error ? "border-2 border-red-500" : ""
+				}`}
 				type={type}
 				name={name}
 				id={name}
 				placeholder={label}
-				value={value}
-				onChange={onChange}
-				onBlur={onBlur}
-				aria-invalid={ariaInvalid ? "true" : "false"}
-				aria-describedby={ariaDescribedby}
 				{...props}
 			/>
 			{error && (
 				<p
-					className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block"
+					className="mt-2 text-sm text-red-500"
 					role="alert"
 					id={`error-${name}`}
 				>
