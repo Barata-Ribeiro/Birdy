d34ef682a5210b4ad75c890b8f3ccb7c84ce9a1e
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-03 08:16:02 -0300

    fix: fix typing to accept null values for testing empty fields only

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,21 +1,26 @@
 import { ChangeEvent, useState } from "react";
 
-interface ValidationTypes {
-	[key: string]: {
-		regex: RegExp;
-		message: string;
-	};
+type FormType = "username" | "email" | "password" | "number" | null;
+
+interface TypeRules {
+	regex: RegExp;
+	message: string;
 }
 
-const types: ValidationTypes = {
+const types: Record<Exclude<FormType, null>, TypeRules> = {
+	username: {
+		regex:
+			/^[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]([a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ ]{3,23})[a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]$/,
+		message:
+			"Username must start with a letter, be 5-25 characters long, may include spaces and accented letters, but no numbers or special symbols.",
+	},
 	email: {
 		regex:
 			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 		message: "Use a valid email address.",
 	},
 	password: {
-		regex:
-			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+		regex: /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
 		message:
 			"The password must have at least 8 characters, one uppercase letter, one lowercase letter, one number, and one special character.",
 	},
@@ -25,16 +30,15 @@ const types: ValidationTypes = {
 	},
 };
 
-const useForm = (type: string | false) => {
+const useForm = (type: FormType = null) => {
 	const [value, setValue] = useState<string>("");
 	const [error, setError] = useState<string | null>(null);
 
 	const validate = (value: string): boolean => {
-		if (type === false) return true;
-		if (value.length === 0) {
-			setError("There must be a value.");
+		if (value.length <= 0) {
+			setError("This field can not be empty.");
 			return false;
-		} else if (types[type] && !types[type].regex.test(value)) {
+		} else if (type !== null && types[type] && !types[type].regex.test(value)) {
 			setError(types[type].message);
 			return false;
 		} else {
