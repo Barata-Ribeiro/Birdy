ca5304043ad13f514e171a80ae9442ab9e2019d5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-17 07:51:52 -0300

    feat: add 'Profile Stats' slice reducer

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -6,17 +6,19 @@ import feed from "./slices/feed.slice";
 import photo from "./slices/photo.slice";
 import photoPost from "./slices/photoPost.slice";
 import profile from "./slices/profile.slice";
+import profileStats from "./slices/stats.slice";
 import token from "./slices/token.slice";
 import user from "./slices/user.slice";
 
 const persistConfig = {
 	key: "root",
 	storage,
-	blacklist: ["feed", "photo", "profile", "token"],
+	blacklist: ["feed", "photo", "profile", "profileStats", "token"],
 };
 
 const rootReducer = combineReducers({
 	profile,
+	profileStats,
 	photo,
 	photoPost,
 	token,
