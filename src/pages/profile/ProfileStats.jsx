158e24ee78da6166808f0b179b267236cb9b3fff
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2023-11-17 07:52:11 -0300

    feat: removed mocked data to implement real data

--- src/pages/profile/ProfileStats.jsx
+++ src/pages/profile/ProfileStats.jsx
@@ -1,49 +1,58 @@
 import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import {
-	VictoryBar,
-	VictoryChart,
-	VictoryGroup,
-	VictoryLabel,
-	VictoryPie,
+  VictoryBar,
+  VictoryChart,
+  VictoryGroup,
+  VictoryLabel,
+  VictoryPie,
 } from "victory";
+import Error from "../../components/helpers/Error";
 import Head from "../../components/helpers/Head";
+import Loading from "../../components/helpers/Loading";
+import { fetchProfileStats } from "../../store/slices/stats.slice";
 
 const ProfileStats = () => {
+	const { token } = useSelector((state) => state.token.data);
+	const { data, loading, error } = useSelector((state) => state.profileStats);
+
+	const dispatch = useDispatch();
+
 	const [hitsData, setHitsData] = useState([]);
 	const [commentsData, setCommentsData] = useState([]);
 	const [likesData, setLikesData] = useState([]);
 	const [pieChartData, setPieChartData] = useState([]);
 
 	useEffect(() => {
-		const mockData = [];
-		const sampleImages = ["Image1", "Image2", "Image3", "Image4"];
-
-		for (let i = 0; i < sampleImages.length; i++) {
-			mockData.push({
-				title: sampleImages[i],
-				hits: Math.floor(Math.random() * 10000),
-				comments: Math.floor(Math.random() * 1000),
-				likes: Math.floor(Math.random() * 5000),
-			});
+		if (!data) {
+			dispatch(fetchProfileStats(token));
+			return;
 		}
 
-		const hitsGraphData = mockData.map((item) => ({
+		const photosArray = data.photos.map((photo) => ({
+			title: photo.title,
+			hits: photo.meta.total_hits,
+			comments: photo.meta.total_comments,
+			likes: photo.meta.total_likes,
+		}));
+
+		const hitsGraphData = photosArray.map((item) => ({
 			x: item.title,
 			y: item.hits,
 			label: "Hits",
 		}));
-		const commentsGraphData = mockData.map((item) => ({
+		const commentsGraphData = photosArray.map((item) => ({
 			x: item.title,
 			y: item.comments,
 			label: "Comments",
 		}));
-		const likesGraphData = mockData.map((item) => ({
+		const likesGraphData = photosArray.map((item) => ({
 			x: item.title,
 			y: item.likes,
 			label: "Likes",
 		}));
 
-		const pieGraphData = mockData.map((item) => ({
+		const pieGraphData = photosArray.map((item) => ({
 			x: item.title,
 			y: item.hits,
 			label: item.title,
@@ -53,7 +62,26 @@ const ProfileStats = () => {
 		setCommentsData(commentsGraphData);
 		setLikesData(likesGraphData);
 		setPieChartData(pieGraphData);
-	}, []);
+	}, [data, dispatch, token]);
+
+	if (loading) return <Loading />;
+	if (error) return <Error error={error} />;
+	if (!data) return null;
+
+	if (data.photos.length <= 0) {
+		return (
+			<section className="p-4 sm:px-0">
+				<Head title="Stats" description="View your profile stats here." />
+				<div className="my-10 text-center">
+					<h1 className="text-2xl font-semibold">Your Stats</h1>
+					<p className="mt-4 text-lg text-gray-600">
+						No photos to generate stats. Start sharing your moments to see
+						statistics!
+					</p>
+				</div>
+			</section>
+		);
+	}
 
 	return (
 		<section className="p-4 sm:px-0">
@@ -64,27 +92,27 @@ const ProfileStats = () => {
 
 			<h1 className="text-center text-2xl">Your Stats</h1>
 			<article className="my-4 grid grid-cols-1 gap-6 lg:grid-cols-2">
-				<div className="font-heading col-span-full flex justify-around rounded-lg p-4 font-medium shadow-lg">
+				<div className="col-span-full flex justify-around rounded-lg p-4 font-heading font-medium shadow-lg">
 					<p>
 						Total Hits:{" "}
 						<span className="font-body font-normal">
-							{Math.floor(Math.random() * 100000)}
+							{hitsData.reduce((acc, item) => acc + item.y, 0)}
 						</span>
 					</p>
 					<p>
 						Total Comments:{" "}
 						<span className="font-body font-normal">
-							{Math.floor(Math.random() * 100000)}
+							{commentsData.reduce((acc, item) => acc + item.y, 0)}
 						</span>
 					</p>
 					<p>
 						Total Likes:{" "}
 						<span className="font-body font-normal">
-							{Math.floor(Math.random() * 100000)}
+							{likesData.reduce((acc, item) => acc + item.y, 0)}
 						</span>
 					</p>
 				</div>
-				<div className="font-heading col-auto rounded-lg p-4 font-medium shadow-lg">
+				<div className="col-auto rounded-lg p-4 font-heading font-medium shadow-lg">
 					<VictoryPie
 						data={pieChartData}
 						colorScale={"qualitative"}
@@ -108,7 +136,7 @@ const ProfileStats = () => {
 					/>
 				</div>
 
-				<div className="font-heading col-auto rounded-lg p-4 font-medium shadow-lg">
+				<div className="col-auto rounded-lg p-4 font-heading font-medium shadow-lg">
 					<VictoryChart domainPadding={30}>
 						<VictoryGroup
 							offset={15}
