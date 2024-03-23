f6d881cc52fa6c188c08ec1a60acc80f31b8a317
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-23 08:10:39 -0300

    feat: add utils functions

---
+++ server/src/utils/operation-functions.ts
@@ -0,0 +1,53 @@
+import {
+    JsonWebTokenError,
+    JwtPayload,
+    TokenExpiredError,
+    verify
+} from "jsonwebtoken"
+import { ObjectLiteral, QueryFailedError, Repository } from "typeorm"
+import {
+    ConflictError,
+    InternalServerError,
+    NotFoundError,
+    UnauthorizedError
+} from "../middleware/helpers/ApiErrors"
+import { isUUIDValid } from "./validity-functions"
+
+export function attemptToGetUserIdFromToken(
+    token: string,
+    key: string
+): string {
+    try {
+        const payload = verify(token, key) as JwtPayload
+        const { id } = payload
+
+        if (!id) throw new NotFoundError("ID not found in token.")
+        if (!isUUIDValid(id))
+            throw new UnauthorizedError("Invalid ID in token.")
+
+        return id
+    } catch (error) {
+        if (error instanceof TokenExpiredError)
+            throw new UnauthorizedError("Your token has expired.")
+        else if (error instanceof JsonWebTokenError)
+            throw new UnauthorizedError("Invalid token.")
+        else throw error
+    }
+}
+
+export async function saveEntityToDatabase<T extends ObjectLiteral>(
+    repository: Repository<T>,
+    entity: T
+): Promise<T> {
+    try {
+        const result = await repository.save(entity)
+        return result
+    } catch (error) {
+        console.error(`Error saving the ${entity} in repository: `, error)
+        if (error instanceof QueryFailedError)
+            throw new ConflictError("Duplicate field value entered.")
+        throw new InternalServerError(
+            `An error occurred while saving the entity ${entity}.`
+        )
+    }
+}
