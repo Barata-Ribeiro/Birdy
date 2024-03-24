473af3f0943911cadcb185e270e6dfa9b5ed865b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-24 07:13:46 -0300

    feat: add UserController with its first routes in UserRoutes

---
+++ server/src/controller/UserController.ts
@@ -0,0 +1,57 @@
+import type { Request, Response } from "express"
+import { SimpleUserResponseDTO } from "../dto/SimpleUserResponseDTO"
+import { BadRequestError } from "../middleware/helpers/ApiErrors"
+import { userRepository } from "../repository/UserRepository"
+import { UserService } from "../service/UserService"
+
+export class UserController {
+    private userService: UserService
+
+    constructor() {
+        this.userService = new UserService()
+    }
+
+    public async getAllUsers(_req: Request, res: Response) {
+        const users = await userRepository.find()
+        const response = users.map((user) =>
+            SimpleUserResponseDTO.fromEntity(user)
+        )
+
+        return res.status(200).json({
+            status: "success",
+            message: "Users fetched successfully.",
+            data: response
+        })
+    }
+
+    public async getUserProfile(req: Request, res: Response) {
+        const { username } = req.params
+        if (!username) throw new BadRequestError("Username is required.")
+
+        const response = await this.userService.getUserProfile(username)
+
+        return res.status(200).json({
+            status: "success",
+            message: "User profile fetched successfully.",
+            data: response
+        })
+    }
+
+    public async getPrivateProfile(req: Request, res: Response) {
+        const { userId } = req.params
+        if (!userId) throw new BadRequestError("User ID is required.")
+
+        if (req.user.data?.id !== userId)
+            throw new BadRequestError(
+                "You are not authorized to view this profile."
+            )
+
+        const response = await this.userService.getPrivateProfile(userId)
+
+        return res.status(200).json({
+            status: "success",
+            message: "Your profile fetched successfully.",
+            data: response
+        })
+    }
+}
--- server/src/v1/router/UserRoutes.ts
+++ server/src/v1/router/UserRoutes.ts
@@ -1,11 +1,19 @@
 import { Router } from "express"
+import { UserController } from "../../controller/UserController"
 import authMiddleware from "../../middleware/AuthMiddleware"
 
 const routes = Router()
+const userController = new UserController()
 
-routes.get("/", (req, res, next) => {})
+routes.get("/", (req, res, next) =>
+    userController.getAllUsers(req, res).catch(next)
+)
 
-routes.get("/:userId", authMiddleware, (req, res, next) => {})
+routes.get("/profile/:username", (req, res, next) =>
+    userController.getUserProfile(req, res).catch(next)
+)
+
+routes.get("/me/:userId", authMiddleware, (req, res, next) => {})
 
 routes.put("/:userId", authMiddleware, (req, res, next) => {})
 
