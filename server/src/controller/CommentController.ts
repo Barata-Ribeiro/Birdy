626188b4b9c0f5ce056f9a754a24fadabef2ebc8
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-30 07:59:29 -0300

    feat: implement updateComment functionality

--- server/src/controller/CommentController.ts
+++ server/src/controller/CommentController.ts
@@ -35,11 +35,7 @@ export class CommentController {
         if (!photoId)
             throw new BadRequestError("The photo ID parameter is required.")
 
-        await this.commentService.addComment(
-            user.data,
-            photoId,
-            content
-        )
+        await this.commentService.addComment(user.data, photoId, content)
 
         return res.status(201).json({
             status: "success",
@@ -48,9 +44,28 @@ export class CommentController {
     }
 
     async updateComment(req: Request, res: Response) {
+        const { user } = req
         const { photoId } = req.params
+        const { commentId } = req.params
+        const { content } = req.body
+
+        if (!user.data) throw new UnauthorizedError("User not authenticated.")
         if (!photoId)
             throw new BadRequestError("The photo ID parameter is required.")
+        if (!commentId)
+            throw new BadRequestError("The comment ID parameter is required.")
+
+        await this.commentService.updateComment(
+            user.data,
+            photoId,
+            commentId,
+            content
+        )
+
+        return res.status(200).json({
+            status: "success",
+            message: "Comment updated successfully."
+        })
     }
 
     async deleteComment(req: Request, res: Response) {
--- server/src/service/CommentService.ts
+++ server/src/service/CommentService.ts
@@ -35,7 +35,7 @@ export class CommentService {
         if (!isUUIDValid(String(author.id)) || !isUUIDValid(photoId))
             throw new BadRequestError("Invalid author ID or photo ID.")
 
-        if (!content)
+        if (!content || content.trim() === "")
             throw new BadRequestError("You cannot add an empty comment.")
 
         const checkIfPhotoExists = await photoRepository.existsBy({
@@ -55,5 +55,46 @@ export class CommentService {
         })
 
         await saveEntityToDatabase(commentRepository, newComment)
+    }
+
+    async updateComment(
+        author: Partial<User>,
+        photoId: string,
+        commentId: string,
+        content: string
+    ) {
+        if (!isUUIDValid(String(author.id)) || !isUUIDValid(photoId))
+            throw new BadRequestError("Invalid author ID or photo ID.")
+        if (!isUUIDValid(commentId))
+            throw new BadRequestError("Invalid comment ID.")
+
+        if (!content || content.trim() === "")
+            throw new BadRequestError(
+                "You cannot update a comment to be empty."
+            )
+
+        const checkIfPhotoExists = await photoRepository.existsBy({
+            id: photoId
+        })
+        if (!checkIfPhotoExists) throw new NotFoundError("Photo not found.")
+
+        const checkIfAuthorExists = await commentRepository.existsBy({
+            id: author.id
+        })
+        if (!checkIfAuthorExists) throw new NotFoundError("User not found.")
+
+        const comment = await commentRepository.findOne({
+            where: {
+                id: commentId,
+                author: { id: author.id },
+                photo: { id: photoId }
+            }
+        })
+        if (!comment) throw new NotFoundError("Comment not found.")
+
+        comment.content = content.trim()
+        comment.was_edited = true
+
+        await saveEntityToDatabase(commentRepository, comment)
     }
 }
