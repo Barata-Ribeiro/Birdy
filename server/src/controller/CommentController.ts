6e352120de75467e90cd2c30d12edcaa804d50f5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-03-30 07:41:31 -0300

    feat: implement getComments functionality

--- server/src/controller/CommentController.ts
+++ server/src/controller/CommentController.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from "express"
+import { BadRequestError } from "../middleware/helpers/ApiErrors"
 import { CommentService } from "../service/CommentService"
 
 export class CommentController {
@@ -9,18 +10,34 @@ export class CommentController {
     }
 
     async getComments(req: Request, res: Response) {
-        // implementation
+        const { photoId } = req.params
+        if (!photoId)
+            throw new BadRequestError("The photo ID parameter is required.")
+
+        const response = await this.commentService.getComments(photoId)
+
+        return res.status(200).json({
+            status: "success",
+            message: "Comments retrieved successfully.",
+            data: response
+        })
     }
 
     async addComment(req: Request, res: Response) {
-        // implementation
+        const { photoId } = req.params
+        if (!photoId)
+            throw new BadRequestError("The photo ID parameter is required.")
     }
 
     async updateComment(req: Request, res: Response) {
-        // implementation
+        const { photoId } = req.params
+        if (!photoId)
+            throw new BadRequestError("The photo ID parameter is required.")
     }
 
     async deleteComment(req: Request, res: Response) {
-        // implementation
+        const { photoId } = req.params
+        if (!photoId)
+            throw new BadRequestError("The photo ID parameter is required.")
     }
 }
--- server/src/service/CommentService.ts
+++ server/src/service/CommentService.ts
@@ -1,1 +1,30 @@
-export class CommentService {}
+import { BadRequestError } from "../middleware/helpers/ApiErrors"
+import { commentRepository } from "../repository/CommentRepository"
+import { isUUIDValid } from "../utils/validity-functions"
+
+export class CommentService {
+    async getComments(photoId: string) {
+        if (!isUUIDValid(photoId))
+            throw new BadRequestError("Invalid photo ID.")
+
+        const [comments, total] = await commentRepository
+            .createQueryBuilder("comment")
+            .leftJoinAndSelect("comment.author", "author")
+            .select([
+                "comment.id",
+                "comment.content",
+                "comment.was_edited",
+                "comment.createdAt",
+                "comment.updatedAt",
+                "author.id",
+                "author.username",
+                "author.display_name",
+                "author.avatar_url"
+            ])
+            .where("comment.photoId = :photoId", { photoId })
+            .orderBy("comment.createdAt", "DESC")
+            .getManyAndCount()
+
+        return { comments, total }
+    }
+}
