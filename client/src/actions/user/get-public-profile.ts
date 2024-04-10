7aebdefb115204da8eaa9f00bb5061a3c5d4ca34
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-10 06:53:45 -0300

    feat: add 'get public profile' action

---
+++ client/src/actions/user/get-public-profile.ts
@@ -0,0 +1,34 @@
+"use server"
+
+import { ApiResponse, State } from "@/interfaces/actions"
+import { PublicProfileResponse } from "@/interfaces/api/users"
+import ApiError from "@/utils/api-error"
+import { USER_GET_PROFILE } from "@/utils/api-urls"
+
+export default async function getPublicProfile(state: State, username: string) {
+    const URL = USER_GET_PROFILE(username)
+
+    try {
+        const response = await fetch(URL, {
+            method: "GET",
+            next: { revalidate: 60 }
+        })
+
+        const responseData = (await response.json()) as ApiResponse
+
+        if (!response.ok)
+            throw new Error(
+                responseData.message ?? "An unknown error occurred."
+            )
+
+        const data = responseData.data as PublicProfileResponse
+        
+        return {
+            ok: true,
+            client_error: null,
+            response: { ...responseData, data }
+        }
+    } catch (error) {
+        return ApiError(error)
+    }
+}
--- client/src/interfaces/api/users.d.ts
+++ client/src/interfaces/api/users.d.ts
@@ -11,6 +11,19 @@ export interface UserContextResponse {
     updatedAt: string
 }
 
+export interface PublicProfileResponse {
+    id: string
+    username: string
+    display_name: string
+    avatar_url?: string
+    cover_image_url?: string
+    bio?: string
+    photo_count: number
+    liked_photo_count: number
+    following_count: number
+    follower_count: number
+}
+
 export interface PrivateProfileResponse {
     id: string
     username: string
