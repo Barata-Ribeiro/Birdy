2e9c3bf1723e760439e95a12916ca08ec36ccf4b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-12 09:56:31 -0300

    feat: implement base public photo page

--- client/src/app/photo/[slug]/page.tsx
+++ client/src/app/photo/[slug]/page.tsx
@@ -1,7 +1,71 @@
-export default async function PhotoPage() {
+import getPhoto from "@/actions/photos/get-photo"
+import { useUser } from "@/context/user-context"
+import { PhotoResponse } from "@/interfaces/api/photos"
+import Image from "next/image"
+import Link from "next/link"
+import { notFound } from "next/navigation"
+
+interface PhotoPageProps {
+    params: {
+        slug: [photoId: string, slug: string]
+    }
+}
+
+export async function generateMetadata({ params }: PhotoPageProps) {
+    const state = await getPhoto(params.slug[0])
+    const photo = state.response?.data as PhotoResponse | null
+
+    if (!photo) return { title: "Photo", description: "Photo Not Found..." }
+
+    return {
+        title: photo.title + " | Birdy",
+        description: `photo${photo.title} taken by ${photo.author.display_name}.`
+    }
+}
+
+export default async function PhotoPage({ params }: PhotoPageProps) {
+    const state = await getPhoto(params.slug[0])
+    const photo = state.response?.data as PhotoResponse | null
+    if (!photo) return notFound()
+    const { user } = useUser()
+
     return (
-        <main>
-            <h1>Photo</h1>
-        </main>
+        <section className="my-4">
+            <div className="grid grid-cols-1 justify-center gap-4 max-md:items-center max-md:px-1 md:grid-cols-2 lg:justify-between">
+                <Image
+                    src={photo.image_url}
+                    alt={`${photo.title}, by ${photo.author.display_name}`}
+                    title={`${photo.title}, by ${photo.author.display_name}`}
+                    className="object-cover italic md:rounded-xl"
+                    width={1000}
+                    height={1000}
+                    sizes="100vw"
+                    priority
+                />
+                <div className="flex flex-col gap-3 md:gap-5">
+                    {/* Title */}
+                    <div className="flex items-end justify-between">
+                        <div className="mb-2 flex flex-col items-start justify-start gap-1 leading-none">
+                            <h1 className="text-4xl font-semibold">
+                                {photo.title}
+                            </h1>
+                            <Link
+                                className="text-xs text-green-spring-400 dark:text-green-spring-300"
+                                href={`/user/${photo.author.id}/${photo.author.username}`}
+                                aria-label={`Profile Author: ${photo.author.username}`}
+                            >
+                                @{photo.author.username}
+                            </Link>
+                        </div>
+
+                        {/* IMPLEMENT DELETE BUTTON */}
+                        {/* IMPLEMENT DELETE BUTTON */}
+
+                        {/* IMPLEMENT OTHER BUTTONS */}
+                        {/* IMPLEMENT OTHER BUTTONS */}
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
