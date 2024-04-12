df17882d6e48640aaca1b641c1c8fa1dd08f7d6b
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-12 07:16:56 -0300

    feat: add base setup for public profiles

--- client/src/app/user/[...slug]/page.tsx
+++ client/src/app/user/[...slug]/page.tsx
@@ -1,7 +1,31 @@
-export default async function UserPage() {
+import type { Metadata } from "next"
+import { notFound } from "next/navigation"
+
+interface UserPageParams {
+    params: {
+        slug: [userId: string, username: string]
+    }
+}
+
+export async function generateMetadata({
+    params
+}: UserPageParams): Promise<Metadata> {
+    const username = params.slug[1]
+
+    return {
+        title: `${username} | Birdy`,
+        description: `This is the user page of ${username} on Birdy.`
+    }
+}
+
+export default async function UserPage({ params }: UserPageParams) {
+    if (!params.slug) return notFound()
+
     return (
-        <main>
-            <h1>User</h1>
-        </main>
+        <section>
+            <h1>User Page</h1>
+            <p>userId: {params.slug[0]}</p>
+            <p>username: {params.slug[1]}</p>
+        </section>
     )
 }
