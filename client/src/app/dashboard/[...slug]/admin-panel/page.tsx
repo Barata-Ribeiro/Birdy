b7bbc06adcdce7b71047f1ff87afb67201709807
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-12 09:35:20 -0300

    feat: add base for admin panel and delete pages

---
+++ client/src/app/dashboard/[...slug]/admin-panel/page.tsx
@@ -0,0 +1,7 @@
+export default async function AdminPanelPage() {
+    return (
+        <section>
+            <h1>Admin Panel</h1>
+        </section>
+    )
+}
---
+++ client/src/app/dashboard/[...slug]/delete/page.tsx
@@ -0,0 +1,7 @@
+export default async function DeletePage() {
+    return (
+        <section>
+            <h1>Delete</h1>
+        </section>
+    )
+}
