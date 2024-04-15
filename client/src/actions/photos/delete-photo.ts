920db44a2f7b365c02061b1eb6f266a10ec758bd
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-15 06:57:53 -0300

    feat: implement photo delete actions

---
+++ client/src/actions/photos/delete-photo.ts
@@ -0,0 +1,31 @@
+"use server"
+
+import { ApiResponse } from "@/interfaces/actions"
+import ApiError from "@/utils/api-error"
+import { PHOTO_DELETE_PHOTO } from "@/utils/api-urls"
+
+export default async function deletePhoto(photoId: string) {
+    const URL = PHOTO_DELETE_PHOTO(photoId)
+
+    try {
+        const response = await fetch(URL, {
+            method: "DELETE",
+            next: { revalidate: 60, tags: ["photos"] }
+        })
+
+        const responseData = (await response.json()) as ApiResponse
+
+        if (!response.ok)
+            throw new Error(
+                responseData.message ?? "An unknown error occurred."
+            )
+
+        return {
+            ok: true,
+            client_error: null,
+            response: responseData
+        }
+    } catch (error) {
+        return ApiError(error)
+    }
+}
