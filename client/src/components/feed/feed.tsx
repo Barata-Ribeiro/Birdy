512efb07f7c6ae00f1a8baed447086a735a5d97a
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-16 08:09:22 -0300

    feat: add Feed and FeedPhotos components

---
+++ client/src/components/feed/feed.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import getPhotosFeed from "@/actions/photos/get-photos-feed"
+import FeedPhotos from "@/components/feed/photos-feed"
+import Loading from "@/components/utils/loading"
+import { FeedResponse } from "@/interfaces/api/photos"
+import { useEffect, useRef, useState } from "react"
+
+export interface FeedProps {
+    photos: FeedResponse[]
+    userId?: string | null
+}
+
+export default function Feed({ photos, userId }: FeedProps) {
+    const [photosFeed, setPhotosFeed] = useState<FeedResponse[]>(photos)
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
+    const [infinite, setInfinite] = useState(photos.length < 6 ? false : true)
+
+    const fetching = useRef(false)
+
+    function infiniteScroll() {
+        if (fetching.current) return
+
+        fetching.current = true
+
+        setLoading(true)
+
+        setTimeout(() => {
+            setPage((currentPage) => currentPage + 1)
+            fetching.current = false
+            setLoading(false)
+        }, 1000)
+    }
+
+    useEffect(() => {
+        if (page === 1) return
+        async function fetchFeedPhotos(page: number) {
+            const state = await getPhotosFeed(
+                { perPage: 6, page, userId },
+                { cache: "no-store" }
+            )
+            const data = state.response?.data as FeedResponse[] | null
+
+            if (state && data) {
+                setPhotosFeed((prevPhotos) => [...prevPhotos, ...data])
+                if (data.length < 6) setInfinite(false)
+            }
+        }
+
+        fetchFeedPhotos(page)
+    }, [page])
+
+    useEffect(() => {
+        if (infinite) {
+            window.addEventListener("scroll", infiniteScroll)
+            window.addEventListener("wheel", infiniteScroll)
+        } else {
+            window.removeEventListener("scroll", infiniteScroll)
+            window.removeEventListener("wheel", infiniteScroll)
+        }
+        return () => {
+            window.removeEventListener("scroll", infiniteScroll)
+            window.removeEventListener("wheel", infiniteScroll)
+        }
+    }, [infinite])
+
+    return (
+        <>
+            <FeedPhotos photos={photosFeed} />
+            <div className="mx-auto my-1 flex h-24">
+                {infinite ? (
+                    loading && <Loading />
+                ) : (
+                    <p className="pb-0 pl-8 pr-16 pt-4 text-center font-semibold text-green-spring-300">
+                        You have reached the end of the feed.
+                    </p>
+                )}
+            </div>
+        </>
+    )
+}
---
+++ client/src/components/feed/photos-feed.tsx
@@ -0,0 +1,54 @@
+import { FeedResponse } from "@/interfaces/api/photos"
+import Image from "next/image"
+import Link from "next/link"
+import { useState } from "react"
+import { FaEye, FaHeart, FaRegComments } from "react-icons/fa"
+
+export default function FeedPhotos({ photos }: { photos: FeedResponse[] }) {
+    const [hoveredImageId, setHoveredImageId] = useState<string | null>(null)
+
+    return (
+        <ul className="masonry sm:masonry-sm md:masonry-md px-4 sm:px-0 [&>li:not(:first-child)]:mt-4">
+            {photos.map((photo, i) => (
+                <li
+                    key={photo.id + "_" + i}
+                    id={photo.id + "_" + i}
+                    className="break-inside"
+                >
+                    <Link
+                        href={`/photo/${photo.id}`}
+                        className="relative"
+                        onPointerEnter={() => setHoveredImageId(photo.id)}
+                        onPointerLeave={() => setHoveredImageId(null)}
+                    >
+                        <div
+                            className={`absolute inset-0 ${hoveredImageId === photo.id ? "flex" : "hidden"} h-full w-full items-end justify-around gap-4 rounded-md bg-black/60 pb-4 text-mantis-50`}
+                        >
+                            <span className="flex items-center gap-2">
+                                <FaEye />
+                                {photo.meta.total_views}
+                            </span>
+                            <span className="flex items-center gap-2">
+                                <FaHeart />
+                                {photo.meta.total_likes}
+                            </span>
+                            <span className="flex items-center gap-2">
+                                <FaRegComments />
+                                {photo.meta.total_comments}
+                            </span>
+                        </div>
+                        <Image
+                            src={photo.image_url}
+                            alt={`Photo: ${photo.title}, by @${photo.author.username}`}
+                            title={`Photo: ${photo.title}, by @${photo.author.username}`}
+                            className="rounded-md object-cover"
+                            width={1500}
+                            height={1500}
+                            sizes="100vw"
+                        />
+                    </Link>
+                </li>
+            ))}
+        </ul>
+    )
+}
