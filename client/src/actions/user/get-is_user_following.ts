18639903561add6ca1c86767663550c9c0c04324
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-21 09:19:00 -0300

    feat: add isUserFollowing action to check if a user is following another user

---
+++ client/src/actions/user/get-is_user_following.ts
@@ -0,0 +1,35 @@
+"use server"
+
+import { ApiResponse } from "@/interfaces/actions"
+import ApiError from "@/utils/api-error"
+import { USER_CHECK_FOLLOW } from "@/utils/api-urls"
+
+export default async function isUserFollowing(
+    username: string,
+    userId: string
+) {
+    const URL = USER_CHECK_FOLLOW(username, userId)
+    try {
+        const response = await fetch(URL, {
+            method: "GET",
+            next: { revalidate: 60 }
+        })
+
+        const responseData = (await response.json()) as ApiResponse
+
+        if (!response.ok)
+            throw new Error(
+                responseData.message ?? "An unknown error occurred."
+            )
+
+        const data = responseData.data as { followed_by_loggedIn_user: boolean }
+
+        return {
+            ok: true,
+            client_error: null,
+            response: { ...responseData, data }
+        }
+    } catch (error: unknown) {
+        return ApiError(error)
+    }
+}
