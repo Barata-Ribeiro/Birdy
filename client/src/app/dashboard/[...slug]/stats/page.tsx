12328b8b2c045a77f60fdeb09f71e3a94de296a2
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-22 07:50:59 -0300

    feat: add user stats page with dynamic loading and stats display

--- client/src/app/dashboard/[...slug]/stats/page.tsx
+++ client/src/app/dashboard/[...slug]/stats/page.tsx
@@ -1,9 +1,40 @@
+import getUserPhotosStats from "@/actions/user/get-user-photos-stats"
 import { DashboardParams } from "@/app/dashboard/[...slug]/layout"
+import Loading from "@/components/utils/loading"
+import { UserPhotosStatsResponse } from "@/interfaces/api/users"
+import { Metadata } from "next"
+import dynamic from "next/dynamic"
+
+export const metadata: Metadata = {
+    title: "Stats | Birdy",
+    description:
+        "In this page, you have access to your profile stats such as your total hits, likes, and comments."
+}
+
+const DashboardStats = dynamic(
+    () => import("@/components/dashboard/dashboard-stats"),
+    { loading: () => <Loading />, ssr: false }
+)
 
 export default async function StatsPage({ params }: DashboardParams) {
+    const state = await getUserPhotosStats(params.slug[0])
+    const photos = state.response?.data as UserPhotosStatsResponse
+
+    if (!photos) return null
+
     return (
-        <main>
-            <h1>Stats</h1>
-        </main>
+        <section className="p-4 sm:px-0">
+            <h1 className="text-center text-2xl dark:text-mantis-50">
+                Your Stats
+            </h1>
+            {photos.total_photos > 0 ? (
+                <DashboardStats data={photos} />
+            ) : (
+                <p className="mt-4 text-center text-lg text-gray-600 dark:text-mantis-300">
+                    No photos to generate stats. Start sharing your moments to
+                    see statistics!
+                </p>
+            )}
+        </section>
     )
 }
---
+++ client/src/components/dashboard/dashboard-stats.tsx
@@ -0,0 +1,169 @@
+"use client"
+
+import { UserPhotosStatsResponse } from "@/interfaces/api/users"
+import { useEffect, useState } from "react"
+import {
+    VictoryBar,
+    VictoryChart,
+    VictoryGroup,
+    VictoryLabel,
+    VictoryPie
+} from "victory"
+
+interface GraphData {
+    x: string
+    y: number
+    label: string
+}
+
+export default function DashboardStats({
+    data
+}: {
+    data: UserPhotosStatsResponse
+}) {
+    const [views, setViews] = useState<GraphData[]>([])
+    const [likes, setLikes] = useState<GraphData[]>([])
+    const [comments, setComments] = useState<GraphData[]>([])
+    const [pieChart, setPieChart] = useState<GraphData[]>([])
+
+    const joinPhotosArray = data.latest_photos.photos.concat(
+        data.popular_photos.photos
+    )
+
+    useEffect(() => {
+        if (!data) return
+
+        const views = joinPhotosArray.map((photo) => ({
+            x: photo.title,
+            y: photo.meta.total_views,
+            label: "Views"
+        }))
+        const likes = joinPhotosArray.map((photo) => ({
+            x: photo.title,
+            y: photo.meta.total_likes,
+            label: "Likes"
+        }))
+        const comments = joinPhotosArray.map((photo) => ({
+            x: photo.title,
+            y: photo.meta.total_comments,
+            label: "Comments"
+        }))
+        const pieChart = joinPhotosArray.map((photo) => ({
+            x: photo.title,
+            y: photo.meta.total_views,
+            label: photo.title
+        }))
+
+        setViews(views)
+        setLikes(likes)
+        setComments(comments)
+        setPieChart(pieChart)
+    }, [data])
+
+    return (
+        <section className="my-4 grid grid-cols-1 gap-6 lg:grid-cols-2">
+            <div className="font-heading col-span-full flex justify-around rounded-lg p-4 font-medium shadow-lg dark:bg-green-spring-200 dark:text-green-spring-950">
+                <p>
+                    Total Hits:{" "}
+                    <span className="font-body font-normal">
+                        {views.reduce((acc, item) => acc + item.y, 0)}
+                    </span>
+                </p>
+                <p>
+                    Total Comments:{" "}
+                    <span className="font-body font-normal">
+                        {comments.reduce((acc, item) => acc + item.y, 0)}
+                    </span>
+                </p>
+                <p>
+                    Total Likes:{" "}
+                    <span className="font-body font-normal">
+                        {likes.reduce((acc, item) => acc + item.y, 0)}
+                    </span>
+                </p>
+            </div>
+            <div className="font-heading col-auto rounded-lg p-4 font-medium shadow-lg dark:bg-green-spring-200">
+                <VictoryPie
+                    data={pieChart}
+                    colorScale={"qualitative"}
+                    innerRadius={50}
+                    padding={{ top: 20, bottom: 20, left: 80, right: 80 }}
+                    animate={{
+                        duration: 2000,
+                        onLoad: { duration: 1000 }
+                    }}
+                    style={{
+                        data: {
+                            fillOpacity: 0.9,
+                            stroke: "hsl(100, 6%, 10%)",
+                            strokeWidth: 2
+                        },
+                        labels: { fontSize: 14 }
+                    }}
+                />
+            </div>
+
+            <div className="font-heading col-auto rounded-lg p-4 font-medium shadow-lg dark:bg-green-spring-200">
+                <VictoryChart domainPadding={30}>
+                    <VictoryGroup
+                        offset={15}
+                        colorScale={"qualitative"}
+                        animate={{
+                            duration: 2000,
+                            onLoad: { duration: 1000 }
+                        }}
+                    >
+                        <VictoryBar
+                            data={views}
+                            barWidth={12}
+                            labelComponent={
+                                <VictoryLabel
+                                    style={{ fontSize: 12 }}
+                                    angle={90}
+                                    dx="-5"
+                                    verticalAnchor="start"
+                                    textAnchor="end"
+                                />
+                            }
+                            style={{
+                                data: { fillOpacity: 0.9, strokeWidth: 1 }
+                            }}
+                        />
+                        <VictoryBar
+                            data={comments}
+                            barWidth={12}
+                            labelComponent={
+                                <VictoryLabel
+                                    style={{ fontSize: 12 }}
+                                    angle={90}
+                                    dx="-5"
+                                    verticalAnchor="start"
+                                    textAnchor="end"
+                                />
+                            }
+                            style={{
+                                data: { fillOpacity: 0.9, strokeWidth: 1 }
+                            }}
+                        />
+                        <VictoryBar
+                            data={likes}
+                            barWidth={12}
+                            labelComponent={
+                                <VictoryLabel
+                                    style={{ fontSize: 12 }}
+                                    angle={90}
+                                    dx="-5"
+                                    verticalAnchor="start"
+                                    textAnchor="end"
+                                />
+                            }
+                            style={{
+                                data: { fillOpacity: 0.9, strokeWidth: 1 }
+                            }}
+                        />
+                    </VictoryGroup>
+                </VictoryChart>
+            </div>
+        </section>
+    )
+}
