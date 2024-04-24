6bbd6f5496ace48221b21d1d3e4fa8fd84bc6e61
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-24 07:52:25 -0300

    feat, fix: create dashboard delete component, fix password validation in deletePrivateProfile action

--- client/src/actions/user/delete-private-profile.ts
+++ client/src/actions/user/delete-private-profile.ts
@@ -1,17 +1,36 @@
 "use server"
 
-import { ApiResponse } from "@/interfaces/actions"
+import { ApiResponse, State } from "@/interfaces/actions"
 import ApiError from "@/utils/api-error"
 import { USER_DELETE_PRIVATE_PROFILE } from "@/utils/api-urls"
+import { revalidateTag } from "next/cache"
 import { cookies } from "next/headers"
 
-export default async function deletePrivateProfile(userId: string) {
-    const URL = USER_DELETE_PRIVATE_PROFILE(userId)
+export default async function deletePrivateProfile(
+    state: State,
+    formData: FormData
+) {
+    const userId = formData.get("userId") as string
+    const username = formData.get("username") as string | null
+    const password = formData.get("password") as string | null
+    const confirm_password = formData.get("confirmPassword") as string | null
 
     try {
         const access_token = cookies().get("access_token")?.value
         if (!access_token)
             throw new Error("You must be logged in to view this page.")
+
+        if (!userId) throw new Error("User ID is required.")
+        const URL = USER_DELETE_PRIVATE_PROFILE(userId)
+
+        if (!username)
+            throw new Error("Username is required to delete your profile.")
+
+        if (!password)
+            throw new Error("Password is required to delete your profile.")
+
+        if (confirm_password !== password)
+            throw new Error("Passwords do not match.")
 
         const response = await fetch(URL, {
             method: "DELETE",
@@ -24,6 +43,8 @@ export default async function deletePrivateProfile(userId: string) {
             throw new Error(
                 responseData.message ?? "An unknown error occurred."
             )
+
+        revalidateTag("public-profile")
 
         return {
             ok: true,
---
+++ client/src/components/dashboard/dashboard-delete.tsx
@@ -0,0 +1,104 @@
+"use client"
+
+import deletePrivateProfile from "@/actions/user/delete-private-profile"
+import FormButton from "@/components/utils/form-button"
+import Input from "@/components/utils/input"
+import { UserContextResponse } from "@/interfaces/api/users"
+import { useEffect } from "react"
+import { useFormState, useFormStatus } from "react-dom"
+
+export default function DashboardDelete({
+    data
+}: {
+    data: UserContextResponse
+}) {
+    const { pending } = useFormStatus()
+    const [state, action] = useFormState(deletePrivateProfile, {
+        ok: false,
+        client_error: null,
+        response: null
+    })
+
+    useEffect(() => {
+        if (state.ok) window.location.href = "/"
+    }, [state.ok])
+
+    return (
+        <>
+            <p className="text-center">
+                Are you sure you want to delete your account? This action cannot
+                be undone.
+            </p>
+            <p className="text-center">
+                All your data will be{" "}
+                <span className="font-bold">permanently deleted</span>. If you
+                are sure you want to delete your account, please enter the
+                required information below and click the "Delete Account"
+                button. If you are not sure, you can click the "Cancel" button
+                to return to your dashboard.
+            </p>
+            <form
+                action={action}
+                className="group mx-auto w-full px-4 sm:w-2/3 lg:px-0"
+            >
+                <div className="pb-2 pt-4">
+                    <Input
+                        label="Username"
+                        className="bg-mantis-200 p-4 text-lg placeholder:text-green-spring-400 dark:bg-mantis-800 dark:placeholder:text-green-spring-300"
+                        required
+                        aria-required
+                    />
+                </div>
+                <div className="pb-2 pt-4">
+                    <Input
+                        type="password"
+                        label="Password"
+                        autoComplete="current-password"
+                        className="bg-mantis-200 p-4 text-lg placeholder:text-green-spring-400 dark:bg-mantis-800 dark:placeholder:text-green-spring-300"
+                        required
+                        aria-required
+                    />
+                </div>
+                <div className="pb-2 pt-4">
+                    <Input
+                        type="password"
+                        label="Confirm Password"
+                        name="confirmPassword"
+                        autoComplete="off"
+                        className="bg-mantis-200 p-4 text-lg placeholder:text-green-spring-400 dark:bg-mantis-800 dark:placeholder:text-green-spring-300"
+                        required
+                        aria-required
+                    />
+                </div>
+                <div className="flex justify-center gap-2 pt-4 sm:justify-end">
+                    <input
+                        type="hidden"
+                        name="userId"
+                        id="userId"
+                        value={data.id}
+                    />
+                    <FormButton
+                        type="submit"
+                        className="rounded !bg-red-600 px-4 py-2 hover:!bg-red-400 sm:!w-fit"
+                        aria-label="Delete Account"
+                        disabled={pending}
+                        aria-disabled={pending}
+                    >
+                        {pending ? "Deleting..." : "Delete Account"}
+                    </FormButton>
+                    <FormButton
+                        type="button"
+                        onClick={() =>
+                            (window.location.href = `/dashboard/${data.id}/${data.username}`)
+                        }
+                        className="dark:!hover:bg-green-spring-700 rounded !bg-green-spring-300 px-4 py-2 hover:!bg-green-spring-400 dark:!bg-green-spring-600 sm:!w-fit"
+                        disabled={pending}
+                        aria-disabled={pending}
+                    >
+                        Cancel
+                    </FormButton>
+                </div>
+            </form>
+        </>
+    )
+}
