e9f00526b3e8f0b8279cb48b6b2380d836e090a5
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-04-24 07:52:53 -0300

    feat: add dashboard delete account page

--- client/src/app/dashboard/[...slug]/delete/page.tsx
+++ client/src/app/dashboard/[...slug]/delete/page.tsx
@@ -1,7 +1,17 @@
-export default async function DeletePage() {
+import { DashboardParams } from "@/app/dashboard/[...slug]/layout"
+import DashboardDelete from "@/components/dashboard/dashboard-delete"
+import { useUser } from "@/context/user-context"
+import { notFound } from "next/navigation"
+
+export default async function DeletePage({ params }: DashboardParams) {
+    const { user } = useUser()
+    if (!user) return notFound()
+    if (params.slug[0] !== user.id) return notFound()
+
     return (
-        <section>
-            <h1>Delete</h1>
+        <section className="p-4 sm:px-0">
+            <h1 className="text-center text-2xl">Delete your account</h1>
+            <DashboardDelete data={user} />
         </section>
     )
 }
