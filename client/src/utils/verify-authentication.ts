3ab07470a9c2694d19a7a00861b9ca7e704f8af9
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-06-30 08:55:06 -0300

    refactor: add proper token verification to auth function

--- client/src/utils/verify-authentication.ts
+++ client/src/utils/verify-authentication.ts
@@ -1,5 +1,7 @@
 import { cookies } from "next/headers"
 import logout from "@/actions/auth/logout"
+import verifyToken from "@/utils/verify-token"
+import refreshToken from "@/actions/auth/refresh-token"
 
 export default async function verifyAuthentication() {
     const access_token = cookies().get("access_token")?.value
@@ -8,5 +10,23 @@ export default async function verifyAuthentication() {
         throw new Error("You must be logged in to view this page.")
     }
     
-    return access_token.toString()
+    if (!await verifyToken(access_token)) {
+        const refresh_token = cookies().get("refresh_token")?.value
+        if (!refresh_token) {
+            await logout()
+            throw new Error("You must be logged in to view this page.")
+        }
+        
+        const verifyRefreshToken = await verifyToken(refresh_token)
+        if (!verifyRefreshToken) {
+            await logout()
+            throw new Error("You must be logged in to view this page.")
+        }
+        
+        await refreshToken(refresh_token)
+
+        return cookies().get("access_token")?.value
+    }
+    
+    return access_token
 }
