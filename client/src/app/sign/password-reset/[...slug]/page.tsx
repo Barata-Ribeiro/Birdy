570bc775201a1e8f1384ad2102a475e2d1b9f5f1
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-07-10 06:28:08 -0300

    chore, refactor: add auth lost/reset password basic page styles, move to proper folder

--- client/src/app/sign/password-reset/[...slug]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function PasswordRestPage() {
-    return (
-        <main>
-            <h1>Password Rest</h1>
-        </main>
-    )
-}
--- client/src/app/sign/password-reset/[userId]/[token]/page.tsx
+++ client/src/app/sign/password-reset/[userId]/[token]/page.tsx
@@ -1,16 +1,16 @@
-export default async function PasswordLostPage() {
+export default async function PasswordResetPage() {
     return (
         <>
             <div className="my-6 flex flex-col gap-2">
                 <h1 className="font-sans text-3xl font-semibold leading-normal text-green-spring-50 lg:text-green-spring-950">
-                    Forgot your password?
+                    Reset your password
                 </h1>
                 <h2 className="font-serif text-lg leading-normal text-green-spring-50 lg:text-green-spring-950">
-                    Type your email address and we will send you a link to reset your password.
+                    Enter your new password below
                 </h2>
             </div>
 
-            <form>{/*TODO: Add PasswordLostForm component*/}</form>
+            <form>{/*TODO: Add Reset password component*/}</form>
         </>
     )
 }
