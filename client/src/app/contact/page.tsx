1e5b8ff7856d59f9ad9144405393313acc5306a4
João M J Barata Ribeiro <122732773+Barata-Ribeiro@users.noreply.github.com>
2024-07-21 08:06:55 -0300

    refactor: disable useless contact form (use repo issue)

--- client/src/app/contact/page.tsx
+++ client/src/app/contact/page.tsx
@@ -1,6 +1,6 @@
-import ContactForm from "@/components/forms/contact-form"
 import { Metadata } from "next"
 import Link from "next/link"
+import ContactForm from "@/components/forms/contact-form"
 
 export const metadata: Metadata = {
     title: "Contact",
@@ -12,12 +12,11 @@ export default async function ContactPage() {
         <section className="container mx-auto px-5 py-10">
             <p className="mx-auto mb-4 max-w-3xl leading-relaxed">
                 The Birdy team is always looking for feedback and ways of improving its users experiences. If you have
-                any questions, requests, or suggestions, please contact us using either the contact form bellow or
-                through our{" "}
+                any questions, requests, or suggestions, please contact us using our{" "}
                 <Link
                     href="https://github.com/Barata-Ribeiro/Birdy/issues"
                     target="_blank"
-                    rel="noopener noreferrer"
+                    rel="external noopener noreferrer"
                     className="text-green-spring-50 transition-colors duration-300 hover:text-bright-turquoise-500 hover:underline dark:text-green-spring-400 dark:hover:text-bright-turquoise-200 lg:text-green-spring-600"
                 >
                     issues page
--- client/src/components/forms/contact-form.tsx
+++ client/src/components/forms/contact-form.tsx
@@ -1,83 +1,48 @@
 "use client"
 
 import FormButton from "@/components/utils/form-button"
-import { FormEvent, useState } from "react"
 import Input from "@/components/utils/input"
 
 export default function ContactForm() {
-    const [isLoading, setIsLoading] = useState(false)
-    const [name, setName] = useState<string>("")
-    const [email, setEmail] = useState<string>("")
-    const [messageSubject, setMessageSubject] = useState<string>("")
-    const [message, setMessage] = useState<string>("")
-
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
-        setIsLoading(true)
-
-        setTimeout(() => {
-            alert(
-                `The contact form is currently disabled. Use the repository's issues page! \n\n Submitted information: \n 
-                Name: ${name} \n 
-                Email: ${email} \n
-                Subject: ${messageSubject} \n
-                Message: ${message}`
-            )
-
-            setIsLoading(false)
-        }, 2500)
-    }
-
     return (
-        <form onSubmit={handleSubmit} className="mx-auto flex max-w-md flex-col gap-4">
+        <form className="mx-auto flex max-w-md flex-col gap-4">
             <Input
                 label="Name"
-                name="name"
+                name="messageName"
                 className="bg-mantis-200 p-4 text-lg placeholder:text-green-spring-400 dark:bg-mantis-800 dark:placeholder:text-green-spring-300"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-                aria-required
+                disabled
             />
             <Input
                 type="email"
                 label="Email"
-                name="email"
+                name="messageEmail"
                 className="bg-mantis-200 p-4 text-lg placeholder:text-green-spring-400 dark:bg-mantis-800 dark:placeholder:text-green-spring-300"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                aria-required
+                disabled
             />
             <Input
                 label="Subject"
                 name="messageSubject"
                 className="bg-mantis-200 p-4 text-lg placeholder:text-green-spring-400 dark:bg-mantis-800 dark:placeholder:text-green-spring-300"
-                value={messageSubject}
-                onChange={(e) => setMessageSubject(e.target.value)}
-                required
-                aria-required
+                disabled
             />
+            <label htmlFor="messageBody" className="sr-only capitalize">
+                Message
+            </label>
             <textarea
-                id="message"
-                name="message"
-                className="peer block w-full rounded-lg border-green-spring-50 bg-mantis-200 p-2.5 text-lg text-mantis-950 placeholder:text-green-spring-400 focus:border-bright-turquoise-500 dark:bg-mantis-800 dark:placeholder:text-green-spring-300"
-                rows={5}
+                id="messageBody"
+                name="messageBody"
+                className="peer block w-full resize-none rounded-lg border-green-spring-50 bg-mantis-200 p-2.5 text-lg text-mantis-950 placeholder:text-green-spring-400 focus:border-bright-turquoise-500 dark:bg-mantis-800 dark:placeholder:text-green-spring-300"
+                rows={6}
                 placeholder="Write your message here..."
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                required
-                aria-required
+                disabled
             ></textarea>
             <div className="mt-4">
                 <FormButton
                     type="submit"
-                    className="rounded-2xl p-4 text-lg group-invalid:pointer-events-none group-invalid:opacity-30"
-                    aria-label="Send Message"
-                    disabled={isLoading}
-                    aria-disabled={isLoading}
+                    className="rounded-2xl p-4 text-lg disabled:cursor-default group-invalid:pointer-events-none group-invalid:opacity-30"
+                    disabled
                 >
-                    {isLoading ? "Sending..." : "Send Message"}
+                    Send Message
                 </FormButton>
             </div>
         </form>
